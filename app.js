4ce456af35c89cc8260b6168bc605f476019dd1e
Surya Kiran <tssuryakiran@gmail.com>
2025-09-10 23:29:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+import { PORT } from './config/env.js';
+
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
+
+const app = express();
+
+// Routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription tracker API!');
+});
+
+// Start the server
+const start = async () => {
+  await connectToDatabase();
+
+  app.listen(PORT, () => {
+    console.log(`Subscription tracker API is running on http://localhost:${PORT}`);
+  });
+};
+
+start();
+
+export default app;
