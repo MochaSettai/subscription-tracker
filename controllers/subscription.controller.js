707f38f6a958372f1831aa74e494951af1461a4e
Surya Kiran <tssuryakiran@gmail.com>
2025-09-20 13:13:16 +0100

    email system created

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -68,3 +68,19 @@ export const getUserSubscriptions = async (req, res, next) => {
         next(error);
     }
 }
+
+export const deleteSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findByIdAndDelete(req.params.id);
+
+        if (!subscription) {
+            const error = new Error('Subscription not found');
+            error.status = 404;
+            throw error;
+        }
+
+        res.status(200).json({ success: true, data: subscription });
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import authorize from '../middlewares/auth.middleware.js';
-import { createSubscription, getAllSubscriptions, getSubscriptionDetails, getUserSubscriptions } from '../controllers/subscription.controller.js';
+import { createSubscription, deleteSubscription, getAllSubscriptions, getSubscriptionDetails, getUserSubscriptions } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
@@ -12,7 +12,7 @@ subscriptionRouter.post('/', authorize, createSubscription);
 
 subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
 
-subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
+subscriptionRouter.delete('/:id', authorize, deleteSubscription);
 
 subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
